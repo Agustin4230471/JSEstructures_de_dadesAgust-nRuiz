aea5797850e1b00da540c940bc4a8cf89b3403e8
Agustín <agustinruiz@institutvidreres.cat>
2024-11-25 12:03:09 +0100

    exercici 1

--- src/JSEstructures_de_dadesAgustinRuiz.js
+++ src/JSEstructures_de_dadesAgustinRuiz.js
@@ -3,3 +3,5 @@ let exercici1 = Array.from({ length: 5 }, () => Math.floor(Math.random() * 100))
 let ordenat = exercici1.sort((a,b) => a - b);
 console.log(ordenat);
 console.log(ordenat[0], ordenat[exercici1.length - 1]);
+
+//exercici2 Utilitza l’operador de propagació per crear un altre array usant els valors de l’exercici anterior
